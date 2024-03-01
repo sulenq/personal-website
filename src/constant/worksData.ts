dd05b996e1ad8ad4dd5d5a0ff22a72520801d5a8
sulenq <whyimustwriteit@gmail.com>
2024-03-01 08:38:39 +0700

    add porto web

--- src/constant/worksData.ts
+++ src/constant/worksData.ts
@@ -53,6 +53,29 @@ const worksData: WorksData = {
       ],
       ssScrollLength: 450,
     },
+    {
+      id: 2,
+      category: 0,
+      clientLogo: "/logos/logo.png",
+      clientName: "Myself",
+      title:
+        "Admin untuk Mengelola Situs Web Landing Page, Blog, Online Course",
+      image: "/images/works/2.png",
+      solution:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      narative:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      ss: [
+        "/images/works/2/ss/0.png",
+        "/images/works/2/ss/1.png",
+        "/images/works/2/ss/2.png",
+        "/images/works/2/ss/3.png",
+        "/images/works/2/ss/4.png",
+        "/images/works/2/ss/5.png",
+        "/images/works/2/ss/6.png",
+      ],
+      ssScrollLength: 450,
+    },
   ],
   en: [
     {
@@ -77,18 +100,14 @@ const worksData: WorksData = {
         "/images/works/0/ss/6.png",
         "/images/works/0/ss/7.png",
         "/images/works/0/ss/8.png",
-        "/images/works/0/ss/9.png",
-        "/images/works/0/ss/10.png",
-        "/images/works/0/ss/11.png",
-        "/images/works/0/ss/12.png",
       ],
       ssScrollLength: 450,
     },
     {
       id: 1,
       category: 0,
-      clientLogo: "/images/clients/gerindra.png",
-      clientName: "PARTAI GERINDRA",
+      clientLogo: "/logos/logo.png",
+      clientName: "Myself",
       title: "Just My Old Personal Website",
       image: "/images/works/1.png",
       solution:
@@ -103,6 +122,28 @@ const worksData: WorksData = {
         "/images/works/1/ss/4.png",
         "/images/works/1/ss/5.png",
         "/images/works/1/ss/6.png",
+      ],
+      ssScrollLength: 450,
+    },
+    {
+      id: 2,
+      category: 0,
+      clientLogo: "/logos/logo.png",
+      clientName: "Myself",
+      title: "Admin to Manage Website Landing Pages, Blogs, Online Courses",
+      image: "/images/works/2.png",
+      solution:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      narative:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      ss: [
+        "/images/works/2/ss/0.png",
+        "/images/works/2/ss/1.png",
+        "/images/works/2/ss/2.png",
+        "/images/works/2/ss/3.png",
+        "/images/works/2/ss/4.png",
+        "/images/works/2/ss/5.png",
+        "/images/works/2/ss/6.png",
       ],
       ssScrollLength: 450,
     },
