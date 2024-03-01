b123631b2c2157dbe5d2cfb5d698a8cf1bec1286
sulenq <whyimustwriteit@gmail.com>
2024-03-01 12:33:25 +0700

    styling

--- src/constant/worksData.ts
+++ src/constant/worksData.ts
@@ -11,9 +11,9 @@ const worksData: WorksData = {
         "Platform untuk Transaksi Pengujian Laboratorium dan Dasboard Admin untuk Mengelola Sistem",
       image: "/images/works/0.png",
       solution:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+        "Saya sebagai frontend developer mengerjakan tampilan sesuai dengan desain yang sudah dibuat oleh rekan tim dan juga implementasi request API",
       narative:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+        "Poltekkes meminta sebuah sistem untuk pengujian lab. Jadi aa 2 pengguna yaitu customer dan admin. Customer dapat membuat akun dan mengajukan pengujian laboratorium dan memantau prosesnya. Admin dapat mengelola sistem tersebut seperti metode pengujian, biaya, dashboard dll.",
       ss: [
         "/images/works/0/ss/0.png",
         "/images/works/0/ss/1.png",
@@ -35,13 +35,13 @@ const worksData: WorksData = {
       id: 1,
       category: 0,
       clientLogo: "/images/clients/gerindra.png",
-      clientName: "PARTAI GERINDRA",
+      clientName: "Myself",
       title: "Hanya Personal Website Lama Saya",
       image: "/images/works/1.png",
       solution:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+        "Membuat tampilan rengan react dan chakraUI. Data konten dari personal website tersebut statis dan terdapat 2 versi bahasa (indonesia dan inggris)",
       narative:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+        "Sebagai seorang fresh graduate saya sebagai web developer ingin memiliki personal website. Saat itu pengetahuan react saya masih pemula namun memiliki sense of design yang sudah bagus karena pengalaman saya sebagai desainer grafis dalam 1 tahun. Jadi saya membuat personal website saya dengan kemampuan yang saya miliki saat itu.",
       ss: [
         "/images/works/1/ss/0.png",
         "/images/works/1/ss/1.png",
@@ -57,14 +57,37 @@ const worksData: WorksData = {
       id: 2,
       category: 0,
       clientLogo: "/logos/logo.png",
-      clientName: "Myself",
+      clientName: "Bussiness Watch Indonesia",
       title:
         "Admin untuk Mengelola Situs Web Landing Page, Blog, Online Course",
       image: "/images/works/2.png",
       solution:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
       narative:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+        "Dibutuhkan webapp admin untuk mengelola website-website yang akan dibuat nanti. Aksi seperti membuat, memperbarui, menghapus, restore harus ada dan webapp admin ini juga memiliki role admin tertentu seperti admin online course, admin blog, dll.",
+      ss: [
+        "/images/works/2/ss/0.png",
+        "/images/works/2/ss/1.png",
+        "/images/works/2/ss/2.png",
+        "/images/works/2/ss/3.png",
+        "/images/works/2/ss/4.png",
+        "/images/works/2/ss/5.png",
+        "/images/works/2/ss/6.png",
+      ],
+      ssScrollLength: 450,
+    },
+    {
+      id: 3,
+      category: 0,
+      clientLogo: "/logos/logo.png",
+      clientName: "Bussiness Watch Indonesia",
+      title:
+        "Admin untuk Mengelola Situs Web Landing Page, Blog, Online Course",
+      image: "/images/works/2.png",
+      solution:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+      narative:
+        "Dibutuhkan webapp admin untuk mengelola website-website yang akan dibuat nanti. Aksi seperti membuat, memperbarui, menghapus, restore harus ada dan webapp admin ini juga memiliki role admin tertentu seperti admin online course, admin blog, dll.",
       ss: [
         "/images/works/2/ss/0.png",
         "/images/works/2/ss/1.png",
