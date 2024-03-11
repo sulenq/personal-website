eadce6f121ec6f7d3382840df83ce49bb1f8f638
sulenq <whyimustwriteit@gmail.com>
2024-03-11 10:02:50 +0700

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,8 @@ function ScrollToTop() {
 
 const App = () => {
   useEffect(() => {
+    window.scrollTo(0, 0);
+
     if ("scrollRestoration" in window.history) {
       window.history.scrollRestoration = "manual";
     }
